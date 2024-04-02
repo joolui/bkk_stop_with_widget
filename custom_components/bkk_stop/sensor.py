cff7f07bf01225f565939a01b1ed3e14dcca7c42
attila <atticus.rationalis@gmail.com>
2024-04-02 10:31:13 +0200

    Increase connection timeout to 1 minute

--- custom_components/bkk_stop/sensor.py
+++ custom_components/bkk_stop/sensor.py
@@ -35,7 +35,7 @@ CONF_FAVORITES = 'favorites'
 DEFAULT_NAME = 'Budapest GO'
 DEFAULT_ICON = 'mdi:bus'
 
-HTTP_TIMEOUT = 15 # secs
+HTTP_TIMEOUT = 60 # secs
 MAX_RETRIES = 3
 SCAN_INTERVAL = timedelta(seconds=120)
 
